585f24c1680de2349e9df462db48e79ed5287b24
emmeliab <emmeliabraun@gmail.com>
2022-08-30 12:01:57 -0400

    updated README; added clean data files and data assembly R script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DAT_Proj
+
+# Purpose
+## This repository contains the data and analysis of the DAT/ACi curve data we collected in the Tapajos in Aug 2022
+
+# Data
+* ACi data were collected on 13 trees in the Tapajos National Forest in Brazil accessible from the walkup tower at K67
+* Data were collected using the LI-6800
+* DAT curves were ran right before Traditional Curves on the same leaves; two leaves per tree (typically)
---
+++ Tapajos_DAT_data_assembly.R
@@ -0,0 +1,166 @@
+# Purpose -----------------------------------------------------------------
+
+## This script is for cleaning and assembling the ACi curve data collected at the
+## Tapajos National Forest in Aug 2022 into one file for further analysis
+
+
+
+# Load Packages -----------------------------------------------------------
+
+library(tidyverse)
+#library(googledrive)
+library(readxl)
+
+# Set WD and Load Data ----------------------------------------------------
+
+# ## Code to import one file
+# library(readxl)
+# file_name <- read_excel("2022-08-14-0906_walkup_aci_clean.xlsx", sheet = "Measurements", skip = 14)
+# View(file_name)
+# 
+# #_____________________________________________________________________________
+# ## First, authorize R to access Google Drive
+# drive_auth(email = "eb0067@mix.wvu.edu")
+# drive_user()
+# 
+# #Check the files in your drive
+# drive_find(n_max = 30)
+# 
+# # Make a list of all the Files you want to upload
+# aci_files_cln <- drive_ls(path = 
+#                                # "Albert_lab_ecohydromics/Gas_exchange_Aug_2022/Clean/LI-6800", 
+#                             as_id("https://drive.google.com/drive/u/2/folders/1nSKLtjd69BJWFlSwEl-r6d8mqK3mF-9c"),  
+#                             pattern = "walkup_aci_clean")
+# print(aci_files_cln)
+# 
+# #Make a FOR loop to download all the files
+# #for (i in 1:length(aci_files_cln$name)) {
+#   links <- drive_link(aci_files_cln$name)
+#   files <- drive_download(links[i], overwrite = TRUE)
+#   data <- read_excel(files[[i]], sheet = "Measurements", skip = 14)
+#   #names <- filenames[[i]]
+#   assign(paste0("data_", aci_files_cln$name[i]), data)
+# } 
+
+
+
+
+
+#_________________________________________________________________
+## From directory
+aci_files_nm <- list.files(path = "~/WVU/Ph.D/Tapajos_DAT_Analysis",
+                           pattern = "walkup_aci_clean")
+print(aci_files_nm)
+
+for (i in 1:length(aci_files_nm)) {
+  files1 <- read_xlsx(path = paste0("~/WVU/Ph.D/Tapajos_DAT_Analysis/", aci_files_nm[i]),
+                      sheet = "Measurements", skip = 14)
+  assign(paste0("data_", aci_files_nm[i]), files1)                     
+}
+
+
+
+
+
+# Assemble into one data file ---------------------------------------------
+
+## Make sure all the data have the same columns (in this case, the 6th and 7th are missing
+## some columns)
+Reduce(setdiff, list(colnames(`data_2022-08-06-1020_walkup_aci_clean.xlsx`), 
+        colnames(`data_2022-08-07-0941_walkup_aci_clean.xlsx`),
+        colnames(`data_2022-08-11-0904_walkup_aci_clean.xlsx`),
+        colnames(`data_2022-08-11-0949_walkup_aci_clean.xlsx`),
+        colnames(`data_2022-08-11-1050_walkup_aci_clean.xlsx`),
+        colnames(`data_2022-08-12-0928_walkup_aci_clean.xlsx`),
+        colnames(`data_2022-08-13-1007_walkup_aci_clean.xlsx`),
+        colnames(`data_2022-08-14-0906_walkup_aci_clean.xlsx`))) #finds the unique columns
+
+
+## Make a function to remove all the stability criteria columns, since they are uneven 
+## across files
+rmv_stbl <- function(file_name) {
+  file_name <- file_name[,!grepl(":", colnames(file_name))]
+}
+
+## Rename the different columns and remove stability columns
+`data_2022-08-06-1020_walkup_aci_clean.xlsx` <- `data_2022-08-06-1020_walkup_aci_clean.xlsx` %>% 
+  rename("Leaf_position" = "Leaf_Position") %>% 
+  rename("Data_QC" = "...244") %>% 
+  rmv_stbl()
+`data_2022-08-07-0941_walkup_aci_clean.xlsx` <- `data_2022-08-07-0941_walkup_aci_clean.xlsx` %>% 
+  rename("Tree_Identifier" = "Tree Identifier") %>% 
+  rename("Data_QC" = "...244") %>% 
+  rmv_stbl()
+`data_2022-08-11-0904_walkup_aci_clean.xlsx` <- `data_2022-08-11-0904_walkup_aci_clean.xlsx`%>% 
+  rename("Tree_Identifier" = "Tree Identifier") %>% 
+  rmv_stbl()
+`data_2022-08-11-0949_walkup_aci_clean.xlsx` <- `data_2022-08-11-0949_walkup_aci_clean.xlsx`%>% 
+  rename("Tree_Identifier" = "Tree Identifier") %>% 
+  rmv_stbl()
+`data_2022-08-11-1050_walkup_aci_clean.xlsx` <- `data_2022-08-11-1050_walkup_aci_clean.xlsx`%>% 
+  rename("Tree_Identifier" = "Tree Identifier") %>% 
+  rename("Data_QC" = "...248") %>% 
+  rmv_stbl()
+`data_2022-08-12-0928_walkup_aci_clean.xlsx` <- `data_2022-08-12-0928_walkup_aci_clean.xlsx`%>% 
+  rename("Tree_Identifier" = "Tree Identifier") %>% 
+  rename("Data_QC" = "...248") %>% 
+  rmv_stbl()
+`data_2022-08-13-1007_walkup_aci_clean.xlsx` <- `data_2022-08-13-1007_walkup_aci_clean.xlsx`%>% 
+  rename("Tree_Identifier" = "Tree Identifier") %>% 
+  rename("Data_QC" = "...248") %>% 
+  rmv_stbl()
+`data_2022-08-14-0906_walkup_aci_clean.xlsx` <- `data_2022-08-14-0906_walkup_aci_clean.xlsx`%>% 
+  rename("Tree_Identifier" = "Tree Identifier") %>% 
+  rename("Data_QC" = "...248") %>% 
+  rmv_stbl()
+
+
+## Fix data columns into the same kind of data
+`data_2022-08-06-1020_walkup_aci_clean.xlsx`$"Leaf_position" <- as.numeric(`data_2022-08-06-1020_walkup_aci_clean.xlsx`$"Leaf_position")
+`data_2022-08-07-0941_walkup_aci_clean.xlsx`$"Leaf_position" <- as.numeric(`data_2022-08-07-0941_walkup_aci_clean.xlsx`$"Leaf_position")
+`data_2022-08-11-0904_walkup_aci_clean.xlsx`$"Leaf_position" <- as.numeric(`data_2022-08-11-0904_walkup_aci_clean.xlsx`$"Leaf_position")
+`data_2022-08-11-0949_walkup_aci_clean.xlsx`$"Leaf_position" <- as.numeric(`data_2022-08-11-0949_walkup_aci_clean.xlsx`$"Leaf_position")
+`data_2022-08-11-1050_walkup_aci_clean.xlsx`$"Leaf_position" <- as.numeric(`data_2022-08-11-1050_walkup_aci_clean.xlsx`$"Leaf_position")
+`data_2022-08-12-0928_walkup_aci_clean.xlsx`$"Leaf_position" <- as.numeric(`data_2022-08-12-0928_walkup_aci_clean.xlsx`$"Leaf_position")
+`data_2022-08-13-1007_walkup_aci_clean.xlsx`$"Leaf_position" <- as.numeric(`data_2022-08-13-1007_walkup_aci_clean.xlsx`$"Leaf_position")
+`data_2022-08-14-0906_walkup_aci_clean.xlsx`$"Leaf_position" <- as.numeric(`data_2022-08-14-0906_walkup_aci_clean.xlsx`$"Leaf_position")
+
+
+## Merge the data together
+all_aci.df <- bind_rows(`data_2022-08-06-1020_walkup_aci_clean.xlsx`,
+                        `data_2022-08-07-0941_walkup_aci_clean.xlsx`,
+                        `data_2022-08-11-0904_walkup_aci_clean.xlsx`,
+                        `data_2022-08-11-0949_walkup_aci_clean.xlsx`,
+                        `data_2022-08-11-1050_walkup_aci_clean.xlsx`,
+                        `data_2022-08-12-0928_walkup_aci_clean.xlsx`,
+                        `data_2022-08-13-1007_walkup_aci_clean.xlsx`,
+                        `data_2022-08-14-0906_walkup_aci_clean.xlsx`)
+
+## Clean the data by filtering out excluded points
+all_aci_cln <- filter(all_aci.df, Data_QC == "OK")
+View(all_aci_cln)
+
+
+## Convert important columns to numeric
+all_aci_cln_num <- all_aci_cln %>%
+  mutate(Ci = parse_number(Ci)) %>%
+  mutate(A = parse_number(A)) %>% 
+  mutate(Ca = parse_number(Ca)) %>% 
+  mutate(E = parse_number(E)) %>%
+  mutate(Qin = parse_number(Qin)) %>%
+  mutate(Tleaf = parse_number(Tleaf))
+
+
+## Save the assembled file as a .csv
+write.csv(x = all_aci_cln_num, file = "clean_aci_data_one_file.csv")
+
+
+## Lessons learned:
+### Add a Data_QC column as a user constant and just populate it afterward- this avoids
+### Us accidentally putting the column name in the wrong row
+
+### Absolutely DO NOT mess with user constants/stability criteria in between files
+### At the very least, just double-check that capitalization rules are the same. We 
+### should standardize this just in case we lose our configuration
+
+### It's better to just remove the stability criteria columns rather than trying to deal
