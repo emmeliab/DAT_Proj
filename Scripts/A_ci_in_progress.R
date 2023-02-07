74be03395d98d79ef44b4c82a8f5f8cf3615bb04
Maquelle Garcia <maquelleneves@gmail.com>
2023-02-07 13:47:10 -0800

    updates

--- Scripts/A_ci_in_progress.R
+++ Scripts/A_ci_in_progress.R
@@ -1,9 +1,9 @@
-# Clean work area and close graphics
-rm(list = ls()) # Remove all objects
-graphics.off()  # Remove all graphics
-cat("\014")     # Remove script in windows console
+#---------------Clean work area, from the beginning------------------
+rm(list = ls()) #Remove all objects
+graphics.off()  #Remove all graphics
+cat("\014")     #Remove script in windows console
 
-# Load relevant libraries
+#--------------Load relevant libraries---------------------------------
 library(chron)
 library(grDevices)
 library(optimx)
@@ -13,63 +13,86 @@ library(weathermetrics)
 library(minpack.lm)
 library(readxl)
 
-# Change the working directory and set names for output files
+
+#------------------------------------------ 
+#Change the directory where you want to save your data
 #setwd("/Users/maquellegarcia/Documents/GitHub/DAT_Proj/Results")
-arquivo <-"A_ci_fit_DAT_Tapajos_20230206"
-pdf(file=paste(arquivo, ".pdf", sep=""),height=10,width=20) # Create pdf to store graphs
 
-# Create a matrix to store the column names of the output text file
-output.names     <- matrix (c("Tree_id","DAT","person","Mean.Tleaf","Max.Tleaf",
-                              "Asat", "Amax","Amin", "Ci.at.Asat","E.at.Asat","Ca","A/Ci","Ci/Ca",
-                              "gs.at.Asat","gs.index","Mean.gs","mean.RH","Flow",
-                              "mean.Press","Max.VPD","Ciindex",
-                              "vcmax_Best_Model","Jmax_Best","Rd_Best",
-                              "Best_Vcmax_25C","Best Jmax_25C",
-                              "GammaStar","Kmi","TleafK.Asat"),nrow=1)
-colnames (output.names) <- output.names
-write.table (output.names, paste(arquivo, ".csv", sep=""), append=TRUE, sep=",",
+# Give a name to the files (table and pdf) that will receive the results
+arquivo <-"A_ci_fit_DAT_Tapajos_20230207"
+# creates the pdf file that will receive the graphs
+pdf(file=paste(arquivo, ".pdf", sep=""),height=10,width=20)
+  
+# Creating a matrix that stores the column names of the text file that will receive the results of this script
+  output.names     <- matrix (c("Tree_id","DAT","person","Mean.Tleaf","Max.Tleaf",
+                                "Asat", "Amax","Amin", "Ci.at.Asat","E.at.Asat","Ca","A/Ci","Ci/Ca",
+                                "gs.at.Asat","gs.index","Mean.gs","mean.RH","Flow",
+                                "mean.Press","Max.VPD","Ciindex",
+                                
+                                
+                                "vcmax_Best_Model","Jmax_Best","Rd_Best",
+                                
+                                "Best_Vcmax_25C",
+                                "Best Jmax_25C",
+                                
+                                "GammaStar",
+                                "Kmi", 
+                                "TleafK.Asat"),nrow=1)
+
+  colnames (output.names)	<- output.names
+  write.table (output.names, paste(arquivo, ".csv", sep=""), append=TRUE, sep=",",
              row.names=FALSE, col.names=FALSE)
+  
 
-# Function to fit Farquhar's biochemical model based on A-Ci curves
+## Function for fitting Farquhar's biochemical model based on A-Ci curves 
+## Error estimated as the sum of squared differences between modeled and observed assimilation rates
+## there is a 'penalty function' to keep the switch from Av to Aj close to Ci = 0.7 x Ca (or 28 Pa)
 A_Ci_Fit1  <- function(x){
-    Vcmax <- x[1]    # Variable 1: Vcmax
-    Jmax  <- x[2]    # Variable 2: Jmax
-    Rd    <- x[3]		 # Variable 3: Rd
-    
-    # Electron transport rate (J) when light is not saturating
-    J    <- (Qabs + Jmax - sqrt((Qabs + Jmax)^2 - 4 * curv * Qabs * Jmax))/(2*curv) 
-    aj   <- ifelse(Curve$Pci>=10, J * (Curve$Pci - gstari_Tleaf) / (4*Curve$Pci + 8 * gstari_Tleaf)-Rd,999)
-    av   <- ifelse(Curve$Pci<50, Vcmax * (Curve$Pci - gstari_Tleaf) / (Kmi + Curve$Pci)-Rd, 999)# Carboxylation rate
-    a     <- pmin(av,aj) # Modeled assimilation rate
-    adiff <- sum((Curve$A - a)^2) # Sum of squared differences between modeled and observed assimilation
+  Vcmax <- x[1]    # variable 1
+  Jmax  <- x[2]    # variable 2
+  #TPU   <- x[3]		 # variable 3
+  Rd    <- x[3]		 # variable 4
+  
+  J    <- (Qabs + Jmax - sqrt((Qabs + Jmax)^2 - 4 * curv * Qabs * Jmax))/(2*curv) # electron transport rate when light is not saturating
+  aj   <- ifelse(Curve$Pci>=50, J * (Curve$Pci - gstari_Tleaf) / (4*Curve$Pci + 8 * gstari_Tleaf)-Rd,999)
+  av   <- ifelse(Curve$Pci<50, Vcmax * (Curve$Pci - gstari_Tleaf) / (Kmi + Curve$Pci)-Rd, 999)# carboxylation rate
+  #atpu <- ifelse (Curve$Pci == tail(Curve$Pci,1),3 * TPU -Rd , 999)	          # triose phosphate utilization
+  #if (tail(Curve$A,1) <= Curve$A[length(Curve$A)-1]){
+   # aj   <- ifelse(Curve$Pci>=10 & Curve$Pci< max(Curve$Pci),J * (Curve$Pci - gstari_Tleaf) / (4*Curve$Pci + 8 * gstari_Tleaf)-Rd,999)
+                  #}  
+  a     <- pmin(av,aj)
+  adiff <- sum((Curve$A - a)^2)				 # least-square
+  #bdiff <- sum((Curve$A[Curve$Pci == tail(Curve$Pci,1)] - atpu[Curve$Pci == tail(Curve$Pci,1)])^2)
   #diffsum <- adiff +  abs(28-Cseq[max(head(sort((av-aj)^2, index.return=TRUE)$ix,2))]) + (Rd*(-1))#essa parte adiciona a penalizacao
 }
-# Constants used in the Farquhar's model, not considering mesophyll conductance
-R <- 0.008314 # Gas constant
-R2 <- R * 1000
-Kc <- 40.49 # Michaelis-Menten constant for CO2 (Pa) (Bernacchi et al 2001,2002) or 404.9 microbar von Caemmerer et al. (1994)
-delta_Kc <- 79430 # (J mol-1) from Medlyn et al 2002
-Ko <- 27.84 # Michaelis-Menten constant for CO2 (kPa)(Bernacchi et al 2001,2002) or 278.4 mbar von Caemmerer et al. (1994)
-delta_Ko <- 36380 # (J mol-1) from Medlyn et al 2002
-gstar <- 4.275 # CO2 compensation point (Pa) (Bernacchi et al 2001,2002) or 36.9 microbar von Caemmerer et al. (1994)
-delta_gstar <- 37830 # (J mol-1)
 
-# Other Constants
-curv <- 0.85 # Curvature for calculating J from Evans (1989)
-Ambient.CO2 <- 400
-O2 <- 21 # Estimated Oxygen concentration at chloroplast - (kPa)
-K0 <- 0.388 # for accounting for diffusion of CO2 through the gasket (Licor 6400 only), maybe this part can be removed, as the new machine is already accounting for it
 
-# Function to convert parameters to standard temperature 25°C according to Bernacchi
+###Constants used in the Farquhar´s model ***NOT*** considering mesophyll conductance
+R             <- 0.008314    # Gas constant
+R2            <- R * 1000
+Kc   		      <- 40.49		   # Michaelis-Menten constant for CO2 (Pa) (Bernacchi et al 2001,2002) or 404.9 microbar von Caemmerer et al. (1994)
+delta_Kc 		  <- 79430 	     # (J mol-1) from Medlyn et al 2002
+Ko   		      <- 27.84		   # Michaelis-Menten constant for CO2 (kPa)(Bernacchi et al 2001,2002) or 278.4 mbar von Caemmerer et al. (1994)
+delta_Ko 		  <- 36380	     # (J mol-1) from Medlyn et al 2002
+gstar  		    <- 4.275		   # CO2 compensation point (Pa) (Bernacchi et al 2001,2002) or 36.9 microbar von Caemmerer et al. (1994)
+delta_gstar 	<- 37830	     # (J mol-1)
+
+#Other Constants
+curv          <- 0.85        # Curvature for calculating J from Evans (1989)
+Ambient.CO2   <- 400
+O2  	      	<- 21 		     # Estimated Oxygen concentration at chloroplast - (kPa)
+K0            <- 0.388       # for accounting for diffusion of CO2 through the gasket (Licor 6400 only) maybe we need to remove this part the new machine is alredy accounting for it
+
+
+#### Função para conversão parâmetros para temperatura padrão 25°C, de acordo com Bernacchi
 Temp_Correction <- function (p1,p2,p3) {
-    a <- p1/(exp(26.355 - (65.33 /(R*meanTleafK)))) # Vcmax as in Bernacchi
-    b <- p2/(exp( 17.71 - (43.9  /(R*meanTleafK)))) # this is different from Bernacchi
-    # c <- p3/((exp(21.46 - 53.1   /(R*meanTleafK)))/(1+exp((0.65*meanTleafK-201.8)/(R*meanTleafK))))
-    d <- p3/(exp(18.715 - (46.39 /(R*meanTleafK)))) # Rd as in Bernacchi
-    
-    return(c(a,b,d))
+  a <- p1/(exp(26.355 - (65.33 /(R*meanTleafK))))#Vcmax as in Bernacchi
+  b <- p2/(exp( 17.71 - (43.9  /(R*meanTleafK))))#this is different from Bernacchi
+  #c <- p3/((exp(21.46 - 53.1   /(R*meanTleafK)))/(1+exp((0.65*meanTleafK-201.8)/(R*meanTleafK))))
+  d <- p3/(exp(18.715 - (46.39 /(R*meanTleafK))))#Rd as in bernacchi 
+  
+  return(c(a,b,d))
 }
-
 ### Peaked function Medlin et al. 2002PCE
 ### Ea = Activation Energy, DeltaS = entropy factor = 200, Hd = Deactivation Energy)
 ### updated to Kumarathunge et al 2019, New Phytologist.(222: 768-784) doi: 10.1111/nph.15668
@@ -115,10 +138,11 @@ modeled_points <- function (p1,p2,p3){
 #-----------------------------------------------
 setwd("/Users/maquellegarcia/Documents/GitHub/DAT_Proj/Inputs")
 dir()
-curvas<-read_excel("Aci_no_out.xlsx")
 
+curvas<-read_excel("Aci_no_out.xlsx")
 curvas2<-subset(curvas, Data_QC=="OK")#to exclude weird points 
-curvas1<-subset(curvas, Ci>0)#to avoid negative values 
+curvas1<-subset(curvas2, Ci>0)#to avoid negative values 
+#curvas1<-subset(curvas3, Ci>100)#to avoid values below 100
 
 names(curvas)
   
@@ -155,51 +179,47 @@ names(curvas)
   
  
   # converts Ci from ppm to Pa
-  Curve$Pci <- Curve$Ci * Curve$Pa * 0.001
+  Curve$Pci      <- Curve$Ci * Curve$Pa * 0.001
+
+
+  Tleafk <- Curve$Tleaf +273.15
   
-  # Convert Tleaf to kelvin
-  Tleafk <- Curve$Tleaf + 273.15
-  
-  # Calculates a standardized measure of stomatal conductance change during measurements
+# A standardized measure of how much stomatal conductance changed during the measurements
   gsindex <- (max(Curve$gsw) - min(Curve$gsw)) / max(Curve$gsw)
-  Ciindex <- (max(Curve$Ci) - min(Curve$Ci)) / max(Curve$Ci)
+  Ciindex <- (max(Curve$Ci)- min(Curve$Ci))/max(Curve$Ci)
+
+# Enzymatic kinects at actual leaf temperature
+  Kci_Tleaf	  	<- Kc    * exp((Tleafk-298.15)*delta_Kc/   (298.15*8.314*Tleafk))
+  Koi_Tleaf	   	<- Ko    * exp((Tleafk-298.15)*delta_Ko/   (298.15*8.314*Tleafk))
+  gstari_Tleaf	<- gstar * exp((Tleafk-298.15)*delta_gstar/(298.15*8.314*Tleafk))
+  Kmi			      <- Kci_Tleaf*(1+O2/Koi_Tleaf)
+ # VON CAEMMERER, S. (2013), Steady state models of photosynthesis. Plant Cell Environ, 36: 1617-1630. doi:10.1111/pce.12098
+ # Light absorbed = Incident light * 0.85(1-0.15)/2
+  # PlantEcophys uses 0.24
+ Qabs <- Curve$Qin * 0.85*(1 - 0.15)/2
+ Curve$Qabs <- Curve$Qin * 0.85*(1 - 0.15)/2
   
-  # Enzyme kinetics at actual leaf temperature
-  Kci_Tleaf <- Kc * exp((Tleafk - 298.15) * delta_Kc / (298.15 * 8.314 * Tleafk))
-  Koi_Tleaf <- Ko * exp((Tleafk - 298.15) * delta_Ko / (298.15 * 8.314 * Tleafk))
-  gstari_Tleaf <- gstar * exp((Tleafk - 298.15) * delta_gstar / (298.15 * 8.314 * Tleafk))
-  Kmi <- Kci_Tleaf * (1 + O2 / Koi_Tleaf)
-  
-  # Light absorbed
-  Qabs <- Curve$Qin * 0.85 * (1 - 0.15) / 2
-  Curve$Qabs <- Curve$Qin * 0.85 * (1 - 0.15) / 2
-  
-  # Variables to be exported to final results file
-  Curve$Ci_Ca <- Curve$Ci / Curve$CO2_s
-  Curve$WUE <- Curve$A / Curve$gsw
-  Curve$CUE <- Curve$A / Curve$Ci
-  Curve$VUE <- Curve$gsw / Curve$Ci
-  
-  # Finds the best Ci/Ca ratio (based on an expected ratio of 0.7)
-  find.best <- try(nlsLM(VUE ~ a * Ci^-b, control = nls.control(maxiter = 100, warnOnly = T), data = Curve, start = c(a = .2, b = 1.5)))
-  
-  # If the optimization does not work, select VUE at 350-450 ppm CO2
+#Some variable to be exported to the final results file
+#During a A-Ci curve, there are usually two measurements at 400 ppm CO2 (eg. 400,300,250,200,150,100,400,600,800,1200,1500,2000)
+#sometimes the first one is better, sometimes the second one is better
+#this chooses the best one, based on the expected Ci/Ca ratio of 0.7
+  Curve$Ci_Ca <- Curve$Ci/Curve$CO2_s
+  Curve$WUE   <- Curve$A/Curve$gsw
+  Curve$CUE   <- Curve$A/Curve$Ci
+  Curve$VUE   <- Curve$gsw/Curve$Ci
+
+  # CRef <- if(isTRUE(length(Curve$Ci_Ca[Curve$CO2_r<450 & Curve$CO2_r>350]) >= 1)) which(abs(Curve$Ci_Ca-0.7) == min(abs(Curve$Ci_Ca[Curve$CO2_r<450 & Curve$CO2_r>350]-0.7)))  else NA
+  # CRef <- if(isTRUE(length(Curve$VUE[Curve$CO2_r<450 & Curve$CO2_r>350]) >= 1)) which(Curve$VUE == max(Curve$VUE[Curve$CO2_r<450 & Curve$CO2_r>350]))  else NA
+    
+  find.best <- try(nlsLM(VUE ~ a*Ci^-b,control=nls.control(maxiter = 100,warnOnly=T)  ,data = Curve,start= c(a=.2,b=1.5)))
+  #Res_M1 <- Curve$VUE - (coef(find.best1)[1]*Curve$Ci^-coef(find.best1)[2])
   if (is(find.best, "try-error")) {
-      CRef <- if (isTRUE(length(Curve$VUE[Curve$CO2_r < 450 & Curve$CO2_r > 350]) >= 1)) {
-          which(Curve$VUE == max(Curve$VUE[Curve$CO2_r < 450 & Curve$CO2_r > 350]))
-      } else {
-          NA
-      }
+      CRef <- if(isTRUE(length(Curve$VUE[Curve$CO2_r<450 & Curve$CO2_r>350]) >= 1)) which(Curve$VUE == max(Curve$VUE[Curve$CO2_r<450 & Curve$CO2_r>350]))  else NA
   } else {
-      # If the optimization works, select the minimum residual value at 350-450 ppm CO2
-      CRef <- if (isTRUE(length(Curve$VUE[Curve$CO2_r < 450 & Curve$CO2_r > 350]) >= 1)) {
-          which(abs(resid(find.best)) == min(abs(resid(find.best)[Curve$CO2_r < 450 & Curve$CO2_r > 350])))
-      } else {
-          NA
-      }
-  }
+      CRef <- if(isTRUE(length(Curve$VUE[Curve$CO2_r<450 & Curve$CO2_r>350]) >= 1)) which(abs(resid(find.best)) == min(abs(resid(find.best)[Curve$CO2_r<450 & Curve$CO2_r>350])))  else NA
+  }  
   
-  # Variables to be exported to final results file
+  
   ASat              <- Curve$A[CRef][1]
   Amax              <- max(Curve$A, na.rm=TRUE)  
   Ci.Ca             <- Curve$Ci[CRef][1]/Curve$CO2_s[CRef][1]
@@ -214,8 +234,8 @@ names(curvas)
   # Starting values for the "optim" function, based on relationships between Asat or Amax  and Vcmax, Jmax, TPU, and Rd
   # these are just coefficients from fitted linear regressions derived from previous fittings of TROBIT curves
   MeanA <- mean(Curve$A)
-  start.values1 <- c(MeanA * 7.4318+16.927, MeanA * 6.0787+58.343, 0.8)
-  
+  #start.values1 <- c(MeanA * 7.4318+16.927, MeanA * 6.0787+58.343, 0.8)
+  start.values1 <- c(Amax * 3.9821+2,      Amax * 4.6045+ 7,     Amax * 0.3281+0.1826, 0.8)
 
  # parameters (Vcmax, Jmax, TPU and Rd) minimum and maximum values for the "DEoptim" function
  lower.bound  <- c(-15, -15,-15)     
@@ -228,8 +248,16 @@ names(curvas)
 
 # organizing results from the curve fitting
   fits1     <- summary(if(sum( first[3]<0) < length( first$p1)) subset( first, p3 > 0 ) else  first, order= c(value))[1,c(1:7)]
+  #fits10    <- c(coef(tenth )[-3],coef(tenth)[3])
+  #fits10b   <- c(coef(tenthb )[-3],coef(tenthb)[3])
+  
+  #colnames(df) <- paste(colnames(df),"new",sep="_")
   fits1_algorithim  <- row.names(fits1)
+
   fits1 <- unlist(fits1)
+
+ 
+  
 #################### calculating rates from the curve fitting results in order to build figures
   ## CO2 sequence (will be needed for building the figures)
   Cseq <- seq(0,200,0.5)
@@ -244,12 +272,14 @@ names(curvas)
   Trans_fit1 <- max(head(sort((result_lines_1[,1]-result_lines_1[,2])^2, index.return=TRUE)$ix,2))
   best        <- fits1[1:7]
   
+ 
 ###########################
   Tleaf_k <- Curve$Tleaf +273.15
   meanTleafK    <- mean(Tleafk)
- 
+  
 
-##### temperature conversions to a reference temperature    
+
+  ##### temperature conversions to a reference temperature    
 
   best_Vcmax_25C        <- Vc_peaked_25C(       best[[1]],Ea_V, Delta_V, Ed)
   best_Jmax_25C         <- J_peaked_25C(       best[[2]], Ea_J, Delta_J, Ed)
@@ -302,15 +332,9 @@ names(curvas)
                        mean(Curve$RHcham),mean(Curve$Flow),mean(Curve$Pa),
                        max(Curve$VPDleaf),Ciindex,
                        
-                     
                        
                        best[1:3],
                   
-                       
-                       #Ecophys_Vcmax_25C,
-                       #Ecophys_Jmax_25C,
-                       #Ecophys_Vcmax_25C_R,
-                       #Ecophys_Jmax_25C_R,
                        best_Vcmax_25C,
                        best_Jmax_25C,
                        
